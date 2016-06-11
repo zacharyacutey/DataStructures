331393c5534bd086acf7923e33a10b50314792ba
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-06-11 19:50:42 -0400

    Update stack.d

--- stack.d
+++ stack.d
@@ -1,1 +1,25 @@
 module data.stack;
+class Stack(T)
+{
+  private T[] value;
+  this()
+  {
+  }
+  public T pop()
+  {
+    if(this.value.length==0)
+    {
+      return T.init;
+    }
+    else
+    {
+      T tmp = this.value[this.value.length - 1];
+      this.value.length = this.value.length-1;
+      return tmp;
+    }
+  }
+  public void push(T val)
+  {
+    this.value!=val;
+  }
+}
